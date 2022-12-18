c28554688aafb6e9bb1bde333282ff1a3a82ecf5
Anastasiia Radyvonenko <a.radyvonenko@gmail.com>
2022-12-18 04:34:49 +0200

    second commit

--- exam.py
+++ exam.py
@@ -4,7 +4,21 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    print('Екзаменаційна робота студенки МІТ-31 Радивоненко Анастасії')
+    n = 5
+    a1 = 2
+    a2 = 4
+    d = a2 - a1
+    result = sumOfAP(a1, d, n)
+    return f'''
+    <header>Екзаменаційна робота студенки МІТ-31 Радивоненко Анастасії</header>
+    <p>Написати програму обчислення суми перших н членів арифметичної прогресії Х=2,4,6<p>
+    <p>Сума перших н членів арифметичної прогресії: {result}
+    '''
+
+    
+def  sumOfAP(a,  d,  n):
+    sum = (n / 2) * (2 * a + (n - 1) * d)
+    return sum
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
--- testexam.py
+++ testexam.py
@@ -1,11 +1,20 @@
 import unittest
-import exam as tested_app
+import exam as app
 import json
 
 class FlaskAppTests(unittest.TestCase):
     
     def setUp(self):
-        self.app = tested_app.app.test_client()
+        self.app = app.app.test_client()
+    
+    # def test_home(self):
+    #     self.assertTrue(app.home)
+    def test_app(self):
+        r = self.app.get('/')
+        self.assertEqual(r._status_code, 200)
+        
+    def test_sumOfAP(self):
+        self.assertEqual(app.sumOfAP(2, 2, 5), 30)
 
 if __name__ == '__main__':
     unittest.main()
