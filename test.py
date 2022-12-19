dd254c18599b366cd033e047759cb5daac376665
Anastasiia Radyvonenko <a.radyvonenko@gmail.com>
2022-12-19 11:26:05 +0200

    first commit

--- test.py
+++ test.py
@@ -6,14 +6,12 @@ class FlaskAppTests(unittest.TestCase):
     def setUp(self):
         self.app = app.app.test_client()
     
-    # def test_home(self):
-    #     self.assertTrue(app.home)
     def test_app(self):
         r = self.app.get('/')
         self.assertEqual(r._status_code, 200)
         
     def test_sumOfAP(self):
-        self.assertEqual(app.sumOfAP(2, 2, 5), 30)
+        self.assertEqual(app.sumOfAP(1, 1, 3), 6)
 
 if __name__ == '__main__':
     unittest.main()
